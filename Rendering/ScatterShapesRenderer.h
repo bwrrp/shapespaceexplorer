b8d14790c7868f6e74096b07099943070b239bd5
Stef Busking <stef@scivis.net>
2009-11-11 15:08:11 +0000

    Added skeleton class for ScatterShapes visualization.

---
+++ Rendering/ScatterShapesRenderer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ScatterPlotRenderer.h"
+
+namespace Diverse
+{
+	class ScatterShapesRenderer : public ScatterPlotRenderer
+	{
+	public:
+		typedef ScatterPlotRenderer Superclass;
+
+		ScatterShapesRenderer();
+		virtual ~ScatterShapesRenderer();
+	};
+}
