67f57085a06cdca7144e827f4d9f2f260030afb3
Stef Busking <bwrrp@users.noreply.github.com>
2009-12-03 12:47:40 +0000

    Changed color maps for scatter plot and outlines to prevent confusion. Scatter plot now also uses a linear-Gamma color map.

--- Data/Utility.cpp
+++ Data/Utility.cpp
@@ -6,14 +6,18 @@
 #include <iostream>
 #include <sstream>
 
+#include <cmath>
+
 namespace Diverse
 {
 	namespace Utility
 	{
+		// --------------------------------------------------------------------
 		std::string LoadShader(const std::string &filename)
 		{
 			// TODO: embed shader sources in the app for final version
-			std::string apppath = QApplication::applicationDirPath().toStdString();
+			std::string apppath = 
+				QApplication::applicationDirPath().toStdString();
 			// Check up to 4 levels up in the directory hierarchy
 			std::string searchpath = "/Shaders/" + filename;
 			std::ifstream infile((apppath + searchpath).c_str());
@@ -34,6 +38,36 @@ namespace Diverse
 				std::cerr << "Error loading shader!" << std::endl;
 			}
 			return contents.str();
+		}
+
+		// --------------------------------------------------------------------
+		NQVTK::Vector3 ToLinearGamma(const NQVTK::Vector3 &color)
+		{
+			NQVTK::Vector3 v = (color + NQVTK::Vector3(0.055)) / 1.055;
+			return NQVTK::Vector3(
+				std::pow(v.x, 2.4), 
+				std::pow(v.y, 2.4), 
+				std::pow(v.z, 2.4));
+		}
+
+		// --------------------------------------------------------------------
+		NQVTK::Vector3 ToScreenGamma(const NQVTK::Vector3 &color)
+		{
+			NQVTK::Vector3 v = 1.055 * color;
+			return NQVTK::Vector3(
+				std::pow(v.x, 1.0 / 2.4), 
+				std::pow(v.y, 1.0 / 2.4), 
+				std::pow(v.z, 1.0 / 2.4)) - NQVTK::Vector3(0.055);
+		}
+
+		// --------------------------------------------------------------------
+		NQVTK::Vector3 InterpolateColors(const NQVTK::Vector3 &color1, 
+			const NQVTK::Vector3 &color2, double t)
+		{
+			double invt = 1.0 - t;
+			return ToScreenGamma(
+				invt * ToLinearGamma(color1) + 
+				t * ToLinearGamma(color2));
 		}
 	}
 }
