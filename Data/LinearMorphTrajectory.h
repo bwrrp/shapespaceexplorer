2f84399460bd929b8cdbd4099050b659b0e7173a
Stef Busking <bwrrp@users.noreply.github.com>
2010-05-28 15:24:32 +0200

    Added methods to get the morph's end shapes.

--- Data/LinearMorphTrajectory.h
+++ Data/LinearMorphTrajectory.h
@@ -13,6 +13,9 @@ namespace Diverse
 
 		virtual void GetShapes(itpp::vec &offsets, itpp::mat &shapes) const;
 
+		const itpp::vec &GetShape1() { return shape1; }
+		const itpp::vec &GetShape2() { return shape2; }
+
 	protected:
 		itpp::vec shape1;
 		itpp::vec shape2;
